a51d1d405a42ec273d2414200348393f6e9c9163
novedadesdpsn <novedadesdpsn@gmail.com>
2026-08-27 10:42:00 -0300

    Update integraciones-config.js

--- js/integraciones-config.js
+++ js/integraciones-config.js
@@ -10,8 +10,8 @@
 // ============================================================
 
 const APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec";
-const CARPETA_DRIVE_ID = "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
+const CARPETA_DRIVE_ID = "https://drive.google.com/drive/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD?dmr=1&ec=wgc-drive-%5Bmodule%5D-goto";
 
 const GUARDADO_EN_DRIVE_ACTIVO =
   APPS_SCRIPT_WEBAPP_URL !== "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec" &&
-  CARPETA_DRIVE_ID !== "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
+  CARPETA_DRIVE_ID !== "https://drive.google.com/drive/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD?dmr=1&ec=wgc-drive-%5Bmodule%5D-goto";
