1f6fd46d34eb341dc17d1902598082d4194e71e0
novedadesdpsn <novedadesdpsn@gmail.com>
2026-08-27 10:36:04 -0300

    Add files via upload

---
+++ integraciones-config.js
@@ -0,0 +1,6 @@
+const APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec";
+const CARPETA_DRIVE_ID = "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
+
+const GUARDADO_EN_DRIVE_ACTIVO =
+  APPS_SCRIPT_WEBAPP_URL !== "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec" &&
+  CARPETA_DRIVE_ID !== "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
