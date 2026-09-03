0076356190cc47cb4b1c13efe8cbcb692ec57aec
novedadesdpsn <novedadesdpsn@gmail.com>
2026-09-03 08:30:14 -0300

    Update integraciones-config.js

--- js/integraciones-config.js
+++ js/integraciones-config.js
@@ -1,8 +1,8 @@
-const APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec";
+const APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzfkS7FC4Q3T44Uy_rp36jEXxuv80hPFo2Vd-CVIPUEimroRosN-8XznetTQD4QcIKq3A/exec";
 const CARPETA_DRIVE_ID = "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
 
 const GUARDADO_EN_DRIVE_ACTIVO =
-  APPS_SCRIPT_WEBAPP_URL !== "https://script.google.com/macros/s/AKfycbwHDXNIPqC1kGx8vLwJqd2i0fCgbjf0iY_74b3XxOviWNQu_VXdFj6y4oZTgtnIHNmZRQ/exec" &&
+  APPS_SCRIPT_WEBAPP_URL !== "https://script.google.com/macros/s/AKfycbzfkS7FC4Q3T44Uy_rp36jEXxuv80hPFo2Vd-CVIPUEimroRosN-8XznetTQD4QcIKq3A/exec" &&
   CARPETA_DRIVE_ID !== "https://drive.google.com/drive/u/2/folders/1GaqmTYmW5R8T5c9prozdVPBY68PoxxlD";
 
 
